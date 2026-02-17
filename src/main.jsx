0a302097e2aa660ab4f1eda7156b5645e46ce84d
Raju-ahamed <rajuahamed7320@gmail.com>
2026-02-17 13:20:13 +0600

    maps

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { APIProvider, Map } from '@vis.gl/react-google-maps'
+import { mapApiKey } from './map-api-key.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <APIProvider apiKey={mapApiKey}>
+      <Map
+        style={{ width: '100vw', height: '100vh' }}
+        defaultCenter={{ lat: 22.54992, lng: 0 }}
+        defaultZoom={3}
+        gestureHandling='greedy'
+        disableDefaultUI
+      />
+      <App/>
+    </APIProvider>
   </StrictMode>,
 )
